dd39b289bbf73a4c1f0d2fbd12f64bd82156e46a
keeran97 <keeran@BN418202.UoA.auckland.ac.nz>
2023-07-07 17:12:08 +1200

    Add comments to help explain the code.

--- map_comparison.py
+++ map_comparison.py
@@ -9,21 +9,14 @@ import openpyxl as op
 import xlsxwriter as xw
 import xlsxwriter.exceptions
 
-
-def fileTypes():
-
-    filetypes = (
-        ('JSON files', '*.json'),
-        ('All files', '*.*')
-    )
-
-    return filetypes
-##############################
-
+## Define a function to load JSON files.
 def loadJSON():
 
     file_path = fd.askopenfilename(
-        filetypes=fileTypes()
+        filetypes= (
+            ('JSON files', '*.json'),
+            ('All files', '*.*')
+        )
     )
 
     file = open(file_path)
@@ -32,34 +25,41 @@ def loadJSON():
     return file_data
 ##############################
 
+## Define a function to save a file in the JSON format.
 def saveFile(data):
 
     save_path = fd.asksaveasfilename(
-        filetypes = fileTypes(),
+        filetypes = (
+            ('JSON files', '*.json'),
+            ('All files', '*.*')
+        ),
         defaultextension = '.json'
     )
     with open(save_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 ##############################
 
+## Define a function to sort a dictionary by the label's alphabetical order.
 def sortFn(dict):
 
     return dict['label']
 ##############################
 
-def getDifference(file1, file2):
+## Define a function to input two lists and extract the differences between the two.
+def getDifference(list1, list2):
     i = 0
     j = 0
     diff = 0
     differences = []
 
-    while i < len(file1):
-        while j < len(file2):
-            if i == len(file1):
+# Loop comparing the two lists.
+    while i < len(list1):
+        while j < len(list2):
+            if i == len(list1):
                 break
             else:
-                x = file1[i]
-                y = file2[j]
+                x = list1[i]
+                y = list2[j]
 
                 if x['term'] == y['term']:
                     i += 1
@@ -70,29 +70,32 @@ def getDifference(file1, file2):
                     j += 1
 
         if diff > 0:
-            differences.append(file1[i])
+            differences.append(list1[i])
 
         i += 1
         diff = 0
         j = 0
 
+# Sort the list by the label's alphabetical order.
     differences.sort(key=sortFn)
 
     return differences
 ##############################
 
-def getSimilar(file1, file2):
+## Define a function to input two lists and extract the similarities between the two.
+def getSimilar(list1, list2):
     i = 0
     j = 0
     similarities = []
 
-    while i < len(file1):
-        while j < len(file2):
-            x = file1[i]
-            y = file2[j]
+# Loop to extract similarities.
+    while i < len(list1):
+        while j < len(list2):
+            x = list1[i]
+            y = list2[j]
 
             if x['term'] == y['term']:
-                similarities.append(file1[i])
+                similarities.append(list1[i])
                 j += 1
             else:
                 j += 1
@@ -100,47 +103,56 @@ def getSimilar(file1, file2):
         i += 1
         j = 0
 
+# Sort the list by the label's alphabetical order.
     similarities.sort(key=sortFn)
 
     return similarities
 ##############################
 
-def comparison(task, file1, file2, duplicates=True):
-    list = []
-    new_list = []
+## Define a function that inputs two lists, whether the differences or similarities are to be extracted.
+## Note that the 'duplicates' argument is whether to remove duplicates within the list or not.
+def comparison(task, list1, list2, duplicates=True):
 
+# Need to ensure the desired task is input.
+# This will be modified to account for wrong inputs.
     if task == 'differences':
-        list = getDifference(file1, file2)
+        listWithDuplicates = getDifference(list1, list2)
     elif task == 'similarities':
-        list = getSimilar(file1, file2)
-    else:
-        print('Error')
+        listWithDuplicates = getSimilar(list1, list2)
 
     if duplicates == True:
-        new_list = removeDuplicate(list)
-        return new_list
+        listWithoutDuplicates = removeDuplicate(listWithDuplicates)
+        return listWithoutDuplicates
     else:
-        return list
+        return listWithDuplicates
 ##############################
 
+## Define a function to write out to an excel spreadsheet.
+## The required arguments are the worksheet name and data.
+## Optional arguments are a header, the row and column start point, and the titles of the 3 data columns.
+## The 'duplicates' argument is whether the number of occurences of the entry is printed out or not (WIP).
 def writeSheet(worksheet, data, header=None, row=0, col=0, A1Title='Label', B1Title='Term', C1Title='ID', duplicates=False):
 
     ws = wb.add_worksheet(worksheet)
 
+# The header will be printed in the desired start column.
     if header != None:
-        ws.write(0, 0, header)
+        ws.write(0, col, header)
         col += 1
 
+# Write out the titles of the data column.
     ws.write(0, col, A1Title)
     ws.write(0, col + 1, B1Title)
     ws.write(0, col + 2, C1Title)
 
+# Write out the title for the count of duplicates, if included.
     if duplicates == True:
         ws.write(0, col + 3, 'Duplicates')
 
     i = 0
     row = 1
 
+# Loop to write out the data.
     while i < len(data):
         x = data[i]
         id = x['id']
@@ -161,26 +173,35 @@ def writeSheet(worksheet, data, header=None, row=0, col=0, A1Title='Label', B1Ti
     return
 ##############################
 
+## Define a function to write out to an existing excel spreadsheet.
+## The required arguments are the worksheet name, data, and starting row and column.
+## The optional arguments are the header name and data titles.
+## The duplicates argument is to whether to include the count of duplicates in the data or not.
 def addToSheet(worksheet, data, row, col, header=None, A1Title='Label', B1Title='Term', C1Title='ID', duplicates=False):
 
     ws = wb.get_worksheet_by_name(worksheet)
 
+# Write out the header in the starting column.
     if header != None:
         ws.write(0, col, header)
         col += 1
 
-    if row == 0:
-        row += 1
-
+# Write out the data titles.
     ws.write(0, col, A1Title)
     ws.write(0, col + 1, B1Title)
     ws.write(0, col + 2, C1Title)
 
+# Ensure the data titles in row 0 are not overwritten.
+    if row == 0:
+        row += 1
+
+# Write out the title for the count of duplicates, if included.
     if duplicates == True:
         ws.write(0, col + 3, 'Duplicates')
 
     i = 0
 
+# Loop to write out data.
     while i < len(data):
         x = data[i]
         id = x['id']
@@ -201,6 +222,7 @@ def addToSheet(worksheet, data, row, col, header=None, A1Title='Label', B1Title=
     return
 ##############################
 
+## Define a function to remove duplicates within a list.
 def removeDuplicate(list):
 
     i = 0
@@ -223,6 +245,7 @@ def removeDuplicate(list):
             else:
                 j += 1
 
+# The numbers of duplicates have been counted and stored within the list, if needed.
         x['duplicates'] = count
         i += 1
         j = 0
@@ -231,10 +254,14 @@ def removeDuplicate(list):
     return list
 ##############################
 
+## Load the map data. In JSON format.
+## This section will need to be tweaked to increase or decrease the files read.
 human = loadJSON()
 rat = loadJSON()
 fc = loadJSON()
 
+## Extract the required data. Whether it is differences or similarities.
+## This section will need to be tweaked depending on what is to be written out to the excel file.
 diff1 = comparison('differences', human, fc)
 diff2 = comparison('differences', fc, human)
 #diff3 = comparison('differences', rat, fc)
@@ -251,6 +278,7 @@ sim1 = comparison('similarities', human, fc)
 #needR = removeDuplicate(diff5+diff4)
 ###############################
 
+## Prompt the user to select the directory and filename for the excel file.
 filename = fd.asksaveasfilename(
         filetypes = (
             ('xlsx files', '*.xlsx'),
@@ -262,6 +290,8 @@ filename = fd.asksaveasfilename(
 
 wb = xw.Workbook(filename)
 
+## Write out the data to excel sheets.
+## This section will need to be tweaked depending on what is to be written out to the excel file.
 writeSheet('Present in all maps', sim1)
 
 writeSheet('Present in AC Human Male', diff1, header='Not in FC')
