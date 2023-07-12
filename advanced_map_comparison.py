ffffff570f482b1cdcb0faff80feee0b3ccce288
keeran97 <keeran@BN418202.UoA.auckland.ac.nz>
2023-07-12 15:24:58 +1200

    Code can now compare more than 2 map inputs

--- advanced_map_comparison.py
+++ advanced_map_comparison.py
@@ -5,14 +5,14 @@ from tkinter import simpledialog as sd
 from tkinter import messagebox as mb
 import xlsxwriter as xw
 
+
 ## Define a function to load JSON files
 def loadJSON(mapname):
-
-# Prompt a window to show what needs to be done.
+    # Prompt a window to show what needs to be done.
     mb.showinfo(title='Open JSON File', message=('Choose {} file'.format(mapname)))
 
     file_path = fd.askopenfilename(
-        filetypes= (
+        filetypes=(
             ('JSON files', '*.json'),
             ('All files', '*.*')
         )
@@ -22,30 +22,34 @@ def loadJSON(mapname):
     file_data = json.load(file)
 
     return file_data
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
 
 ## Define a function to save a file in the JSON format
 def saveFile(data):
-
-# Prompt a window to show what needs to be done.
-    mb.showinfo(title='Save JSON File', message=('Choose directory to save JSON file'))
+    # Prompt a window to show what needs to be done.
+    mb.showinfo(title='Save JSON File', message='Choose directory to save JSON file')
 
     save_path = fd.asksaveasfilename(
-        filetypes = (
+        filetypes=(
             ('JSON files', '*.json'),
             ('All files', '*.*')
         ),
-        defaultextension = '.json'
+        defaultextension='.json'
     )
     with open(save_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
 
 ## Define a function to sort a dictionary by the label's alphabetical order
 def sortFn(dict):
-
     return dict['label']
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
 
 ## Define a function to input two lists and extract the differences between the two
 def getDifference(list1, list2):
@@ -54,7 +58,7 @@ def getDifference(list1, list2):
     diff = 0
     differences = []
 
-# Loop comparing the two lists.
+    # Loop comparing the two lists.
     while i < len(list1):
         while j < len(list2):
             if i == len(list1):
@@ -78,11 +82,13 @@ def getDifference(list1, list2):
         diff = 0
         j = 0
 
-# Sort the list by the label's alphabetical order.
+    # Sort the list by the label's alphabetical order.
     differences.sort(key=sortFn)
 
     return differences
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
 
 ## Define a function to input two lists and extract the similarities between the two
 def getSimilar(list1, list2):
@@ -90,7 +96,7 @@ def getSimilar(list1, list2):
     j = 0
     similarities = []
 
-# Loop to extract similarities.
+    # Loop to extract similarities.
     while i < len(list1):
         while j < len(list2):
             x = list1[i]
@@ -105,31 +111,37 @@ def getSimilar(list1, list2):
         i += 1
         j = 0
 
-# Sort the list by the label's alphabetical order.
+    # Sort the list by the label's alphabetical order.
     similarities.sort(key=sortFn)
 
     return similarities
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
 
 ## Define a function that inputs two lists, whether the differences or similarities are to be extracted
 ## Note that the 'duplicates' argument is whether to remove duplicates within the list or not
-def comparison(
-        task, list1, list2, duplicates=True
-):
+def comparison(task, list1, list2, duplicates=True):
 
-# Need to ensure the desired task is input.
-# This will be modified to account for wrong inputs.
-    if task == 'differences':
-        listWithDuplicates = getDifference(list1, list2)
-    elif task == 'similarities':
-        listWithDuplicates = getSimilar(list1, list2)
+    dict = {}
 
-    if duplicates == True:
-        listWithoutDuplicates = removeDuplicate(listWithDuplicates)
-        return listWithoutDuplicates
-    else:
-        return listWithDuplicates
-#________________________________________________________________________________________________________________#
+    if duplicates:
+        if task == 'differences':
+            dict['diff{}'.format(1)] = removeDuplicate(getDifference(list1, list2))
+            dict['diff{}'.format(2)] = removeDuplicate(getDifference(list2, list1))
+        elif task == 'similarities':
+            dict['sim{}'.format(1)] = removeDuplicate(getSimilar(list1, list2))
+        else:
+            if task == 'differences':
+                dict['diff{}'.format(1)] = getDifference(list1, list2)
+                dict['diff{}'.format(2)] = getDifference(list2, list1)
+            elif task == 'similarities':
+                dict['sim{}'.format(1)] = getSimilar(list1, list2)
+
+    return dict
+
+
+# ________________________________________________________________________________________________________________#
 
 ## Define a function to write out to an Excel spreadsheet
 ## The required arguments are the worksheet name and data
@@ -141,22 +153,22 @@ def writeSheet(
 
     ws = wb.add_worksheet(worksheet)
 
-# Add bold format
+    # Add bold format
     bold = wb.add_format({'bold': True})
 
-# Add format to change font colour to bold red and size column width.
+    # Add format to change font colour to bold red and size column width.
     red = wb.add_format({'bold': True, 'font_color': 'red'})
 
-# The header will be printed in the desired start column.
-    if header != None:
+    # The header will be printed in the desired start column.
+    if header is not None:
         ws.write(0, col, header.upper(), red)
         if len(header) > 17:
-           ws.set_column(0, col, 30)
+            ws.set_column(0, col, 30)
         else:
             ws.set_column(0, col, 22)
         col += 1
 
-# Write out the data titles and size column widths.
+    # Write out the data titles and size column widths.
     ws.write(0, col, A1Title, bold)
     ws.set_column(col, col, 35)
     ws.write(0, col + 1, B1Title, bold)
@@ -164,14 +176,14 @@ def writeSheet(
     ws.write(0, col + 2, C1Title, bold)
     ws.set_column((col + 2), (col + 2), 15)
 
-# Write out the title for the count of duplicates, if included.
-    if duplicates == True:
+    # Write out the title for the count of duplicates, if included.
+    if duplicates:
         ws.write(0, col + 3, 'Duplicates', bold)
 
     i = 0
     row = 1
 
-# Loop to write out the data.
+    # Loop to write out the data.
     while i < len(data):
         x = data[i]
         id = x['id']
@@ -182,7 +194,7 @@ def writeSheet(
         ws.write(row, col + 1, term)
         ws.write(row, col + 2, id)
 
-        if duplicates == True:
+        if duplicates:
             duplicate = x['duplicates']
             ws.write(row, col + 3, duplicate)
 
@@ -190,7 +202,9 @@ def writeSheet(
         i += 1
 
     return
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
 
 ## Define a function to write out to an existing Excel spreadsheet
 ## The required arguments are the worksheet name, data, and starting row and column
@@ -199,26 +213,25 @@ def writeSheet(
 def addToSheet(
         worksheet, data, row, col, header=None, A1Title='Label', B1Title='Term', C1Title='ID', duplicates=False
 ):
-
     ws = wb.get_worksheet_by_name(worksheet)
 
-# Add bold format for headers and titles
+    # Add bold format for headers and titles
     bold = wb.add_format({'bold': True})
 
-# Add format to change font colour to red.
+    # Add format to change font colour to red.
     red = wb.add_format({'bold': True, 'font_color': 'red'})
 
-# Write out the header in the starting column.
-    if header != None:
+    # Write out the header in the starting column.
+    if header is not None:
         ws.write(0, col, header.upper(), red)
         if len(header) > 16:
-           ws.set_column(0, col, 30)
+            ws.set_column(0, col, 30)
         else:
             ws.set_column(0, col, 22)
 
         col += 1
 
-# Write out the data titles and size columns.
+    # Write out the data titles and size columns.
     ws.write(0, col, A1Title, bold)
     ws.set_column(col, col, 35)
     ws.write(0, col + 1, B1Title, bold)
@@ -226,18 +239,18 @@ def addToSheet(
     ws.write(0, col + 2, C1Title, bold)
     ws.set_column((col + 2), (col + 2), 15)
 
-# Ensure the data titles in row 0 are not overwritten.
+    # Ensure the data titles in row 0 are not overwritten.
     if row == 0:
         row += 1
 
-# Write out the title for the count of duplicates, if included.
+    # Write out the title for the count of duplicates, if included.
     if duplicates == True:
         ws.write(0, col + 3, 'Duplicates', bold)
-        ws.set_column((col+3), (col+3), 10)
+        ws.set_column((col + 3), (col + 3), 10)
 
     i = 0
 
-# Loop to write out data.
+    # Loop to write out data.
     while i < len(data):
         x = data[i]
         id = x['id']
@@ -256,11 +269,12 @@ def addToSheet(
         i += 1
 
     return
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
 
 ## Define a function to remove duplicates within a list
 def removeDuplicate(list):
-
     i = 0
     j = 0
     count = 0
@@ -281,14 +295,18 @@ def removeDuplicate(list):
             else:
                 j += 1
 
-# The numbers of duplicates have been counted and stored within the list, if needed.
+        # The numbers of duplicates have been counted and stored within the list, if needed.
         x['duplicates'] = count
         i += 1
         j = 0
         count = 0
 
     return list
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
+
+## Define a function to print number in its ordinal form.
 def ordinal(n: int):
     if 11 <= (n % 100) <= 13:
         suffix = 'th'
@@ -296,7 +314,11 @@ def ordinal(n: int):
         suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
 
         return str(n) + suffix
-#________________________________________________________________________________________________________________#
+
+
+# ________________________________________________________________________________________________________________#
+
+## Define function to store user input from a button in a variable and kill the window/mainloop.
 def desire(input):
     global option
     option = input
@@ -306,27 +328,25 @@ def desire(input):
 
     return option
 
-#________________________________________________________________________________________________________________#
-def comparisonOption():
-    win = Toplevel()
-    win.title('Desired comparison')
-    Label(win, text='What comparison would you like?').pack()
-    Button(win, text='Differences').pack()
 
+# ________________________________________________________________________________________________________________#
 
-#________________________________________________________________________________________________________________#
+## With the functions defined, the code below takes in JSON files and requires user input to output an Excel
+## spreadsheet with the desired data.
+
+## Establish root window for user prompts.
+root = Tk()
+root.geometry('+500+250')
+root.withdraw()
+# ________________________________________________________________________________________________________________#
 
 ## Load maps.
 
 # Prompt user to input desired number of maps.
 quantity = int(sd.askstring('Number of maps', 'How many maps are to be loaded? (NOTE: WIP - max. 2 maps)'))
 
-# Stick to 2 maps for now.
-while quantity > 2:
-    quantity = int(sd.askstring('Number of maps', 'How many maps are to be loaded? (NOTE: WIP - max. 2 maps)'))
-
 i = 1
-m = {}
+allMaps = {}
 names = {}
 
 # Loop through desired number of maps to input name
@@ -335,30 +355,27 @@ while i <= quantity:
     question = ('What is the name of the {} map?'.format(n))
     map = sd.askstring('Map name', question)
 
-# Use the loadJSON function to load each map.
-    for x in range(i , (i + 1)):
-        names["name{}".format(x)] = map
-        map.replace(" ", "_")
-        m["map{}".format(x)] = loadJSON(map)
+    # Use the loadJSON function to load each map.
+    for x in range(i, (i + 1)):
+        names['name{}'.format(x)] = map
+        map.replace(' ', '_')
+        allMaps['map{}'.format(x)] = loadJSON(map)
         break
 
     i += 1
-
-#________________________________________________________________________________________________________________#
+# ________________________________________________________________________________________________________________#
 
 ## Extract the required data. Whether it is differences or similarities.
 
 # Prompt user input for what is desired - differences, similarities, or both.
-root = Tk()
 win = Toplevel()
-win.geometry('300x175')
+win.geometry('300x175+500+250')
 win.title('Desired comparison')
 Label(win, text='What comparison would you like?').pack()
 Button(win, text='Differences', command=lambda *args: desire('differences')).pack()
 Button(win, text='Similarities', command=lambda *args: desire('similarities')).pack()
 Button(win, text='Both', command=lambda *args: desire('both')).pack()
 
-root.withdraw()
 win.mainloop()
 
 # Execute the desired process.
@@ -367,35 +384,46 @@ z = 0
 d = {}
 s = {}
 
-
-for p in range(i, (i+1)):
-    list1 = list(m.keys())[z]
-    list2 = list(m.keys())[z + 1]
+while z < (len(allMaps) - 1):
+    list1 = list(allMaps.keys())[z]
+    list2 = list(allMaps.keys())[z + 1]
     if option == 'differences':
-        d["diff{0}".format(p)] = comparison('differences', m[list1], m[list2])
-        d["diff{0}".format(p+1)] = comparison('differences', m[list2], m[list1])
+        d.update(comparison('differences', allMaps[list1], allMaps[list2]))
     elif option == 'similarities':
-        s["sim{0}".format(p)] = comparison('similarities', m[list1], m[list2])
+        s.update(comparison('similarities', allMaps[list1], allMaps[list2]))
     elif option == 'both':
-        d["diff{0}".format(p)] = comparison('differences', m[list1], m[list2])
-        d["diff{0}".format(p + 1)] = comparison('differences', m[list2], m[list1])
-        s["sim{0}".format(p)] = comparison('similarities', m[list1], m[list2])
+        d.update(comparison('differences', allMaps[list1], allMaps[list2]))
+        s.update(comparison('similarities', allMaps[list1], allMaps[list2]))
 
-#________________________________________________________________________________________________________________#
+    z += 1
+
+# for p in range(i, (i + 1)):
+#     list1 = list(m.keys())[z]
+#     list2 = list(m.keys())[z + 1]
+#     if option == 'differences':
+#         d['diff{0}'.format(p)] = comparison('differences', m[list1], m[list2])
+#         d['diff{0}'.format(p + 1)] = comparison('differences', m[list2], m[list1])
+#     elif option == 'similarities':
+#         s['sim{0}'.format(p)] = comparison('similarities', m[list1], m[list2])
+#     elif option == 'both':
+#         d['diff{0}'.format(p)] = comparison('differences', m[list1], m[list2])
+#         d['diff{0}'.format(p + 1)] = comparison('differences', m[list2], m[list1])
+#         s['sim{0}'.format(p)] = comparison('similarities', m[list1], m[list2])
+# ________________________________________________________________________________________________________________#
 
 ## Prompt the user to select the directory and filename for the Excel file
 
 # Prompt a window to show what needs to be done.
-mb.showinfo(title='Save excel File', message=('Choose directory and name to save Excel file'))
+mb.showinfo(title='Save excel File', message='Choose directory and name to save Excel file')
 
 filename = fd.asksaveasfilename(
-        filetypes = (
-            ('xlsx files', '*.xlsx'),
-            ('csv files', '*.csv'),
-            ('All files', '*.*')
-        ),
-        defaultextension = '.xlsx'
-    )
+    filetypes=(
+        ('xlsx files', '*.xlsx'),
+        ('csv files', '*.csv'),
+        ('All files', '*.*')
+    ),
+    defaultextension='.xlsx'
+)
 
 wb = xw.Workbook(filename)
 
@@ -414,6 +442,10 @@ elif option == 'both':
     writeSheet('Need to add', d['diff1'], header=('Need to add to {}'.format(names['name2'])))
     addToSheet('Need to add', d['diff2'], header=('Need to add to {}'.format(names['name1'])), row=0, col=4)
 
-
 wb.close()
-#________________________________________________________________________________________________________________#
+# ________________________________________________________________________________________________________________#
+
+## Confirm process.
+
+mb.showinfo(title='2 Map Comparison Complete', message='Done!')
+# ________________________________________________________________________________________________________________#
